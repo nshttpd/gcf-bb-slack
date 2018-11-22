981583eac5d7a790642efa8a5d8ae04666edf271
Steve Brunton <brunton@dudeman.com>
2018-11-22 13:02:34 -0500

    yay, a slack msg post worked

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from flask import abort
 import hashlib
 import hmac
 import os
+import json
+import logging
 from urllib import request, parse
 
 
@@ -18,11 +20,11 @@ def validate_request(body, signature):
     sekret = os.environ.get('BITBUCKET_SECRET', None)
     if sekret is not None:
         s = bytes(sekret, 'utf-8')
-        b = bytes(body, 'utf-8')
-        h = hmac.new(s, b, digestmod=hashlib.sha256).hexdigest()
+        h = hmac.new(s, body, digestmod=hashlib.sha256).hexdigest()
         calc_sig = "sha256=%s" % h
         if calc_sig == signature:
             return True
+    logging.info('got invalid signature')
     return False
 
 
@@ -31,7 +33,7 @@ def send_slack_msg(msg):
     if webhook is not None:
         headers = {'Content-type': 'application/json'}
         data = {'text': msg, 'icon_emoji': ':bitbucket:'}
-        payload = parse.urlencode(data).encode()
+        payload = bytes(json.dumps(data), 'utf-8')
         req = request.Request(webhook, data=payload, headers=headers)
         resp = request.urlopen(req)
     return
@@ -39,13 +41,13 @@ def send_slack_msg(msg):
 
 def get_event_string(event):
     events = {
-        'pullrequest:created': 'PR Created',
+        'pr:opened': 'PR Created',
         'pullrequest:updated': 'PR Updated',
-        'pullrequest:approved': 'PR Approved',
+        'pr:reviewer:approved': 'PR Approved',
         'pullrequest:unapproved': 'PR Unapproved',
         'pullrequest:fulfilled': 'PR Merged',
         'pullrequest:rejected': 'PR Rejected',
-        'pullrequest:comment_created': 'PR Comment Added',
+        'pr:comment:added': 'PR Comment Added',
         'pullrequest:comment_updated': 'PR Comment Updated',
         'pullrequest:comment_deleted': 'PR Comment Deleted'
     }
@@ -55,9 +57,9 @@ def get_event_string(event):
 
 def slack_template(event, d):
     templates = {
-        'pullrequest:created': lambda d : '%s (%d) : %s by %s in %s branch %s' % (d['event_str'], d['pr_id'], d['pr_desc'], d['nick'], d['repo_name'], d['pr_branch']),
-        'pullrequest:approved': lambda d : '%s (%d) : %s (%s | %s) by %s' % (d['event_str'], d['pr_id'], d['pr_desc'], d['pr_branch'], d['repo_nane'], d['approver']),
-        'pullrequest:comment_created': lambda d: '%s (%d) : %s - %s' % (d['event_str'], d['pr_id'], d['nick'], d['comment_text'])
+        'pr:opened': lambda d : '%s (%d) : %s by %s in %s branch %s' % (d['event_str'], d['pr_id'], d['pr_desc'], d['nick'], d['repo_name'], d['pr_branch']),
+        'pr:reviewer:approved': lambda d : '%s (%d) : %s (%s | %s) by %s' % (d['event_str'], d['pr_id'], d['pr_desc'], d['pr_branch'], d['repo_nane'], d['approver']),
+        'pr:comment:added': lambda d: '%s (%d) : %s - %s' % (d['event_str'], d['pr_id'], d['nick'], d['comment_text'])
     }
 
     x = templates.get(event, None)
@@ -67,28 +69,30 @@ def slack_template(event, d):
     return None
 
 
-def bb_webhook(request):
-    event_key = request.headers['x-event-key']
+def bb_webhook(req):
+    event_key = req.headers['x-event-key']
 
     # ping to validate webhook.
     if event_key == 'diagnostics:ping':
         return 'PONG'
 
-    if validate_request(request.data, request.headers['X-Hub-Signature']):
-        if request.method == 'POST':
-            if request.headers['content-type'] == 'application/json':
+    raw_req = req.get_data()
+
+    if validate_request(raw_req, req.headers['X-Hub-Signature']):
+        if req.method == 'POST':
+            if req.headers['content-type'] == 'application/json':
+                req_json = json.loads(raw_req)
                 slack_data = {'event_str': get_event_string(event_key)}
-                req_json = request.get_json()
                 if 'actor' in req_json:
-                    slack_data['nick'] = req_json['actor']['nickname']
-                if 'pullrequest' in req_json:
-                    slack_data['pr_id'] = req_json['pullrequest']['id']
-                    slack_data['pr_desc'] = req_json['pullrequest']['description']
-                    slack_data['pr_branch'] = req_json['pullrequest']['source']['branch']
+                    slack_data['nick'] = req_json['actor']['name']
+                if 'pullRequest' in req_json:
+                    slack_data['pr_id'] = req_json['pullRequest']['id']
+                    slack_data['pr_desc'] = req_json['pullRequest']['title']
+                    slack_data['pr_branch'] = req_json['pullRequest']['fromRef']['displayId']
                 if 'repository' in req_json:
                     slack_data['repo_name'] = req_json['repository']['name']
                 if 'comment' in req_json:
-                    slack_data['comment_text'] = req_json['comment']['content']['raw']
+                    slack_data['comment_text'] = req_json['comment']['text']
                 if 'approval' in req_json:
                     slack_data['approver'] = req_json['approval']['user']['nickname']
 

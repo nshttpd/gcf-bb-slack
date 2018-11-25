aec3260898fd837da504bf1872903bf699c79fcd
Steve Brunton <brunton@dudeman.com>
2018-11-25 17:46:52 -0500

    cleaned up slack message to make more pretty

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import hmac
 import os
 import json
 import logging
-from urllib import request, parse
+from urllib import request
 
 
 #
@@ -32,41 +32,56 @@ def send_slack_msg(msg):
     webhook = os.environ.get('SLACK_WEBHOOK', None)
     if webhook is not None:
         headers = {'Content-type': 'application/json'}
-        data = {'text': msg, 'icon_emoji': ':bitbucket:'}
+        data = {'attachments': [msg], 'icon_emoji': ':bitbucket:'}
         payload = bytes(json.dumps(data), 'utf-8')
         req = request.Request(webhook, data=payload, headers=headers)
         resp = request.urlopen(req)
     return
 
 
-def get_event_string(event):
+def get_attachment_base(event):
     events = {
-        'pr:opened': 'PR Created',
-        'pullrequest:updated': 'PR Updated',
-        'pr:reviewer:approved': 'PR Approved',
-        'pullrequest:unapproved': 'PR Unapproved',
-        'pullrequest:fulfilled': 'PR Merged',
-        'pullrequest:rejected': 'PR Rejected',
-        'pr:comment:added': 'PR Comment Added',
-        'pullrequest:comment_updated': 'PR Comment Updated',
-        'pullrequest:comment_deleted': 'PR Comment Deleted'
+        'pr:opened': {'pretext': 'Pull Request Created', 'color': 'good'},
+        'pr:modified': {'pretext': 'Pull Request Modified', 'color': 'warning'},
+        'pr:reviewer:approved': {'pretext': 'Pull Request Approved', 'color': 'good'},
+        'pr:reviewer:unapproved': {'pretext': 'Pull Request Unapproved', 'color': 'danger'},
+        'pr:reviewer:needs_work': {'pretext': 'Pull Request Needs Work', 'color': 'warning'},
+        'pr:merged': {'pretext': 'Pull Request Merged', 'color': '#000000'},
+        'pr:declined': {'pretext': 'Pull Request Declined', 'color': 'danger'},
+        'pr:comment:added': {'pretext': 'Pull Request Comment Added', 'color': 'good'},
+        'pr:comment:edited': {'pretext': 'Pull Request Comment Edited', 'color': 'warning'},
+        'pr:comment:deleted': {'pretext': 'Pull Request Comment Deleted', 'color': 'danger'}
     }
 
-    return events.get(event, 'Unknown')
+    return events.get(event, None)
 
 
-def slack_template(event, d):
-    templates = {
-        'pr:opened': lambda d : '%s (%d) : %s by %s in %s branch %s' % (d['event_str'], d['pr_id'], d['pr_desc'], d['nick'], d['repo_name'], d['pr_branch']),
-        'pr:reviewer:approved': lambda d : '%s (%d) : %s (%s | %s) by %s' % (d['event_str'], d['pr_id'], d['pr_desc'], d['pr_branch'], d['repo_nane'], d['approver']),
-        'pr:comment:added': lambda d: '%s (%d) : %s - %s' % (d['event_str'], d['pr_id'], d['nick'], d['comment_text'])
-    }
+def slack_template(event_key, d, attachment):
+    bb_host = os.environ.get('BITBUCKET_HOST', None)
 
-    x = templates.get(event, None)
-    if x is not None:
-        return x(d)
+    link = 'https://%s/projects/%s/repos/%s/pull-requests/%s/' % (bb_host,
+                                                                  d['pullRequest']['fromRef']['repository']['project']['key'],
+                                                                  d['pullRequest']['fromRef']['repository']['slug'],
+                                                                  d['pullRequest']['id'])
+    if event_key.startswith('pr:comment'):
+        attachment['text'] = '<%s|#%s> : %s' % (link, d['pullRequest']['id'], d['comment']['text'])
+    else:
+        attachment['text'] = '<%s|#%s> : %s' % (link, d['pullRequest']['id'], d['pullRequest']['title'])
 
-    return None
+    attachment['fields'] = [
+        {
+            'title': 'Author',
+            'value': d['actor']['displayName'],
+            'short': True
+        },
+        {
+            'title': 'Repo : Branch',
+            'value': '%s : %s' % (d['pullRequest']['fromRef']['repository']['slug'], d['pullRequest']['fromRef']['displayId']),
+            'short': True
+        }
+    ]
+
+    return attachment
 
 
 def bb_webhook(req):
@@ -82,24 +97,10 @@ def bb_webhook(req):
         if req.method == 'POST':
             if req.headers['content-type'] == 'application/json':
                 req_json = json.loads(raw_req)
-                slack_data = {'event_str': get_event_string(event_key)}
-                if 'actor' in req_json:
-                    slack_data['nick'] = req_json['actor']['name']
-                if 'pullRequest' in req_json:
-                    slack_data['pr_id'] = req_json['pullRequest']['id']
-                    slack_data['pr_desc'] = req_json['pullRequest']['title']
-                    slack_data['pr_branch'] = req_json['pullRequest']['fromRef']['displayId']
-                if 'repository' in req_json:
-                    slack_data['repo_name'] = req_json['repository']['name']
-                if 'comment' in req_json:
-                    slack_data['comment_text'] = req_json['comment']['text']
-                if 'approval' in req_json:
-                    slack_data['approver'] = req_json['approval']['user']['nickname']
-
-                slack_msg = slack_template(event_key, slack_data)
-
-                if slack_msg is not None:
-                    send_slack_msg(slack_msg)
+                attachment = get_attachment_base(event_key)
+                if attachment is not None:
+                    attachment = slack_template(event_key, req_json, attachment)
+                    send_slack_msg(attachment)
                     return 'OK'
 
     return abort(404)
